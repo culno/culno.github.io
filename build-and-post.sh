cbc0f9ef52b3dc9bc4cafb996933643cbb6a8bf7
culnoty <culnoty@outlook.com>
2017-03-05 18:55:14 +0800

    2017/03/05_18:55:08

--- build-and-post.sh
+++ build-and-post.sh
@@ -1,10 +1,16 @@
+current_date_time="`date +%Y/%m/%d_%H:%M:%S`";
+# commit current project
+git add .
+git commit -m "$current_date_time"
+git push
+
+# commit jekyll site
 jekyll build
 cd ./culno.github.io
 
 echo "current directory:$PWD"
 git add .
 
-current_date_time="`date +%Y/%m/%d_%H:%M:%S`";
 echo "committing $current_date_time"
 
 git commit -m "$current_date_time"
